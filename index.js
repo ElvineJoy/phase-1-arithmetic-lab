87c92e216925093171883d3b9ae8a6ce2564b7de
ElvineJoy <elvinkuiyungest@gmail.com>
2024-03-26 19:16:39 +0300

    arithemetic lab using arithmetic operations in JS and inbuilt objects

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2 
+
+// varaible 2
+const random =Math.floor(Math.random() + 2) * 10;
+Number.isInteger(random);
+
+// Variable 3
+let num3 = 29;
+let num4 = 5;
+
+const mod = num3%num4;
+
+// variable 4
+const max = Math.max(10,11,12,5,19,20);
